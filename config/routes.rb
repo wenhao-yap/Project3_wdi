9ec8d31b4a9a8b5fde87c7b9d4e87259876db9f7
zw-zack <zhengwin94@hotmail.com>
2018-04-26 15:03:39 +0800

    front-end 80% completion

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	resources :queries do
-  		resources :favourites
-	end
-
-	resources :queries
-
 	root 'queries#index'
+	get '/sign-up', to: 'users#new'
+	get '/log-in', to: 'users#login'
+	get '/user/profile', to: 'users#profile'
+	get '/admin/profile', to: 'users#admin'
+
+	devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :queries do
+  	resources :favourites
+  end
+
+  resources :queries
 end
