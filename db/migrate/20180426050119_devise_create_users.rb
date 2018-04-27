0d6d8e0e842be73e904d9c344860b0f4e6444ea7
wenhao <wenhao@freeloops.local>
2018-04-27 16:46:53 +0800

    added admin

--- db/migrate/20180426050119_devise_create_users.rb
+++ db/migrate/20180426050119_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.boolean :admin, null: false, default: "false"
+      t.string :name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2018_04_27_033923) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false, null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -74,7 +76,6 @@ ActiveRecord::Schema.define(version: 2018_04_27_033923) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
