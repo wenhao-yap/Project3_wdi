3183d6beacddfabd3ad5ee51bf362cd2d3903bbc
Edmund <edmundcjj@gmail.com>
2018-04-27 15:25:48 +0800

    Added unfavourite feature for users

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,16 +3,18 @@ class FavouritesController < ApplicationController
     favourites = Favourite.where(user_id: current_user.id)
 
     @favourites = []
-    keys = [:id, :result_id, :name, :price, :img, :url]
+    keys = [:id, :result_id, :name, :price, :platform, :img, :url]
 
     favourites.each do |favourite|
       result = Result.find(favourite.result_id)
       result_id = favourite.result_id
+      next if result.url == nil
       fav = Hash[keys.map {|x| [x,1]}]
       fav[:id] = result.id
       fav[:result_id] = result_id
       fav[:name] = result.name
       fav[:price] = result.price
+      fav[:platform] = result.platform
       fav[:img] = result.img
       fav[:url] = result.url
       @favourites << fav
@@ -31,7 +33,10 @@ class FavouritesController < ApplicationController
   end
 
   def destroy
-
+    favourite_id = Favourite.find_by(result_id: params[:result_id]).id
+    puts "Id to be deleted => #{favourite_id}"
+    Favourite.destroy(favourite_id)
+    redirect_to result_favourites_path
   end
 
   private
