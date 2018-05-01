6d1354abdc11a8006670640424ecb982d7d33ddb
Edmund <edmundcjj@gmail.com>
2018-05-01 15:38:16 +0800

    Added logic for unfavourite in results page and gather data to be displayed in admin dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,7 +3,20 @@ class Admin::DashboardsController < ApplicationController
 
   # Gather data from database to be displayed on dashboard
   def index
-    
+    # Get the popular products for each platform
+    @qoo10_popular_products = PopularProduct.where(platform: 'Qoo10')
+    @lazada_popular_products = PopularProduct.where(platform: 'Lazada')
+    @carousell_popular_products = PopularProduct.where(platform: 'Carousell')
+
+    # Get the total number of items found for searched query for each platform
+    # @qoo10_total_items_found =
+    # @lazada_total_items_found =
+    # @carousell_total_items_found =
+
+    # Get the average price for the searched query for each platform
+    # @qoo10_average_price =
+    # @lazada_average_price =
+    # @carousell_average_price =
   end
 
   	private
@@ -13,4 +26,12 @@ class Admin::DashboardsController < ApplicationController
 			redirect_to root_path
 		end
 	end
+
+  def scrap_qoo10_popular_items_found(qoo10_popular_products)
+
+  end
+
+  def scrap_lazada_popular_items_found(lazada_popular_products)
+
+  end
 end
